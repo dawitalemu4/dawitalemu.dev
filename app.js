b25520a4b3929e06efa117bed1d1c4bec42f43db
Dawit Alemu <106638403+dawitalemu4@users.noreply.github.com>
2023-02-13 22:05:17 -0500

    Update app.js

--- app.js
+++ app.js
@@ -99,7 +99,7 @@ document.addEventListener('mousemove', (e) => {
   })
   });
 
-  
+
   var scrollCounter = 0;
 
   document.addEventListener("wheel", function(event) {
@@ -124,61 +124,73 @@ document.addEventListener('mousemove', (e) => {
       }
   });
 
+ 
+    
+
+    
+
+
   let gif = document.getElementById("game-gif");
-  let gifs = ["hatclose.png", "hatmid.png", "hatopen.png"];
-  let currentimages = 0;
-  let message = document.getElementsByTagName("h5")[0];
-  let gameIntervalId;
-  let startTime;
-  
-  // Start the game
-  startGame();
-  
-  // Add click event listener to the gif
-  gif.addEventListener("click", function() {
-    let now = Date.now();
-    if (now - startTime >= 3500 && now - startTime <= 5000) {
-      message.style.visibility = "hidden";
-      displayWinOutcome();
-    } else {
-      message.style.visibility = "visible";
-      setTimeout(function() {
-        message.style.visibility = "hidden";
-      }, 2000);
-    }
-  });
-  
-  // Add mouseover event listener to the gif
-  gif.addEventListener("mouseover", function() {
-    gif.style.cursor = "pointer";
-  });
-  
-  // Start the game loop
-  function startGame() {
-    message.style.visibility = "hidden";
+let gifs = ["hatclose.png", "hatmid.png", "hatopen.png"];
+let currentimages = 0;
+let message = document.getElementsByTagName("h5")[0];
+let gameIntervalId;
+let startTime;
+
+// Start the game
+startGame();
+
+// Add click and contextmenu event listeners to the gif
+gif.addEventListener("click", onClick);
+gif.addEventListener("contextmenu", onClick);
+
+// Add mouseover event listener to the gif
+gif.addEventListener("mouseover", function() {
+  gif.style.cursor = "pointer";
+});
+
+// Start the game loop
+function startGame() {
+  message.style.visibility = "hidden";
+  startTime = Date.now();
+  gif.src = "luffy fish.gif";
+  gameIntervalId = setInterval(function() {
     startTime = Date.now();
     gif.src = "luffy fish.gif";
-    gameIntervalId = setInterval(function() {
-      startTime = Date.now();
-      gif.src = "luffy fish.gif";
-    }, 5040);
+  }, 5040);
+}
+
+// Handle click event
+function onClick(event) {
+  event.preventDefault();
+  let now = Date.now();
+  if (now - startTime >= 3500 && now - startTime <= 5000) {
+    message.style.visibility = "hidden";
+    displayWinOutcome();
+  } else {
+    message.style.visibility = "visible";
+    setTimeout(function() {
+      message.style.visibility = "hidden";
+    }, 2000);
   }
-  
-  // Display win outcome
-  function displayWinOutcome() {
-    clearInterval(gameIntervalId);
-    let i = 0;
-    gameIntervalId = setInterval(function() {
-      gif.src = gifs[i];
-      i++;
-      if (i === gifs.length) {
-        clearInterval(gameIntervalId);
-        gif.src = gifs[i-1];
-        message.style.visibility = "hidden";
-      }
-      if(i == gifs[1]) {message.style.visibility = "hidden";}
-    }, 400);
-  }
+}
+
+// Display win outcome
+function displayWinOutcome() {
+  clearInterval(gameIntervalId);
+  let i = 0;
+  gameIntervalId = setInterval(function() {
+    gif.src = gifs[i];
+    i++;
+    if (i === gifs.length) {
+      clearInterval(gameIntervalId);
+      gif.src = gifs[i-1];
+      message.style.visibility = "hidden";
+    }
+    if(i == gifs[1]) {message.style.visibility = "hidden";}
+  }, 400);
+}
+
 
  
 
@@ -198,7 +210,11 @@ var textstill = document.getElementById("textstill");
 
 
 
-pokeball.addEventListener("click", function() {
+pokeball.addEventListener("click", handleClick);
+pokeball.addEventListener("contextmenu", handleClick);
+
+function handleClick(event) {
+  event.preventDefault();
   pokeball.style.display = "none";
   pikachujump.style.display = "block";
   setTimeout(function() {
@@ -213,11 +229,12 @@ pokeball.addEventListener("click", function() {
     },3660)
     }, 1500);
   }, 1000);
-});
+};
   
 
 pikachurun.addEventListener("animationend", function() {
   pikachurun.style.display = "none";
   
 });
+
 
